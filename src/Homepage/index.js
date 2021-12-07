423ee24ad93f2cb4ddc489a1a89c43850909f6c2
shashi0d <78725773+shashi0d@users.noreply.github.com>
2021-12-07 20:11:49 +0530

    Added Off Canvas

---
+++ src/Homepage/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Header from "../Header";
+import "./index.css";
+
+const Homepage = () => {
+	return <Header />;
+};
+
+export default Homepage;
---
+++ src/Routes/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "../Homepage";
+const MyRoutes = () => {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" exact element={<Homepage />} />
+			</Routes>
+		</BrowserRouter>
+	);
+};
+
+export default MyRoutes;
