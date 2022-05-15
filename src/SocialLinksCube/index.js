0b890a7589542c6fcb1c5917842c118ebded68cb
shashi0d <78725773+shashi0d@users.noreply.github.com>
2022-05-15 22:29:42 +0530

    adding social media links to ui

--- src/SocialLinksCube/index.js
+++ src/SocialLinksCube/index.js
@@ -1,14 +1,15 @@
 import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react/swiper-react";
-
+import { Link } from "react-router-dom";
 // Import Swiper styles
 import "swiper/swiper-bundle.css";
 
 import "./index.css";
 
 // import required modules
-import { EffectCube, Pagination } from "swiper";
+import { EffectCube, Pagination, Autoplay } from "swiper";
+import SvgComponent from "../SvgComponent";
 
 export default function SocialLinkCube() {
 	return (
@@ -22,22 +23,32 @@ export default function SocialLinkCube() {
 					shadowOffset: 20,
 					shadowScale: 0.94,
 				}}
+				autoplay={{
+					delay: 2500,
+					disableOnInteraction: false,
+				}}
 				loop={true}
 				pagination={true}
-				modules={[EffectCube, Pagination]}
+				modules={[Autoplay, EffectCube, Pagination]}
 				className="mySwiper swipeElement"
 			>
 				<SwiperSlide>
-					<img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+					<Link to={"https://www.instagram.com/shashi096/"} target="_blank" className="m-1">
+						<SvgComponent name="Instagram" />
+					</Link>
+					{/* <img src="https://swiperjs.com/demos/images/nature-1.jpg" /> */}
 				</SwiperSlide>
 				<SwiperSlide>
-					<img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+					<SvgComponent name="Mail" />
+					{/* <img src="https://swiperjs.com/demos/images/nature-2.jpg" /> */}
 				</SwiperSlide>
 				<SwiperSlide>
-					<img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+					<SvgComponent name="Pinterest" />
+					{/* <img src="https://swiperjs.com/demos/images/nature-3.jpg" /> */}
 				</SwiperSlide>
 				<SwiperSlide>
-					<img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+					<SvgComponent name="LinkedIn" />
+					{/* <img src="https://swiperjs.com/demos/images/nature-4.jpg" /> */}
 				</SwiperSlide>
 			</Swiper>
 		</>
